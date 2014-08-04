7dc6193d5a0d5668b4026c0f4c21879f3495e845
It423 <it42@hotmail.co.uk>
2014-08-04 07:43:54 +0100

    Move where the JSON loads

--- Javascript/DrawGraph.js
+++ Javascript/DrawGraph.js
@@ -1,10 +1,21 @@
 var cxt = getContext("canvas");
 
-barGraph(cxt);
+graphData = loadJSON();
+
+if (graphData.graphType.toLowerCase() == "bar") {
+	barGraph(cxt, graphData)
+}
 
 function getContext(canvasID) {
 	var canvas = document.getElementById(canvasID);
 	var context = canvas.getContext('2d');
 
 	return context;
+}
+
+function loadJSON() {
+	$.getJSON('.\\JSON-Graph-Data\\Eye Colour Bar Graph.json', function (data) {
+		console.log(data);
+		return data;
+	});
 }
