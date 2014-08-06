cb98e7a0eb16679d5440ba5fe2bedaebfa84e3f2
It423 <it42@hotmail.co.uk>
2014-08-06 13:40:28 +0100

    add data display

--- Javascript/BarGraph.js
+++ Javascript/BarGraph.js
@@ -1,8 +1,6 @@
 function barGraph(canvas, graphData) {
 	var scaleInfo = calculateScale(graphData, canvas);
 
-	console.log(scaleInfo);
-
 	drawGraph(canvas, graphData, scaleInfo);
 }
 
@@ -37,23 +35,23 @@ function calculateScale(graphData, canvas) {
 
 function workOutBarInfo(canvas, numberOfBars) {
 	// The object to be returned
-	var retunObject = {
+	var returnObj = {
 		"width": 0,
 		"gap": 0
 	};
 
 	// Work out the vars for the return object
-	var graphWidth = canvas.width - 80;
+	var graphWidth = canvas.width - 150;
 	var widthAllowedPerBar = graphWidth / numberOfBars;
 
-	retunObject.width = widthAllowedPerBar - (widthAllowedPerBar / 6);
-	retunObject.gap = widthAllowedPerBar / 6;
+	returnObj.width = widthAllowedPerBar - (widthAllowedPerBar / 6);
+	returnObj.gap = widthAllowedPerBar / 6;
 
-	return retunObject;
+	return returnObj;
 }
 
 function workOutPointInfo(canvas, graphData) {
-	retunObject = {
+	returnObj = {
 		"startY": 0,
 		"endY": 0,
 		"scalePointGap": 0,
@@ -67,21 +65,21 @@ function workOutPointInfo(canvas, graphData) {
 	var firstReading = getFirstYScaleReading(getHighestReading(graphData) - getLowestReading(graphData), 10);
 
 	// Get the start point in the scale, and the last
-	retunObject.startY = firstReading * Math.floor(getLowestReading(graphData) / firstReading);
-	retunObject.endY = firstReading * Math.round(1 + getHighestReading(graphData) / firstReading);
+	returnObj.startY = firstReading * Math.floor(getLowestReading(graphData) / firstReading);
+	returnObj.endY = firstReading * Math.round(1 + getHighestReading(graphData) / firstReading);
 
 	// Get the points
-	for (var i = retunObject.startY; i <= retunObject.endY; i += firstReading) {
-		retunObject.points.push(i);
+	for (var i = returnObj.startY; i <= returnObj.endY; i += firstReading) {
+		returnObj.points.push(i);
 	}
 
-	// The gap between each scale point is 100 pixels
-	retunObject.scalePointGap = 100;
+	// Work out the gap in pixels between each scale point
+	returnObj.scalePointGap = (canvas.height - 100) / (returnObj.points.length - 1);
 
 	// Work out how many pixels there are per a unit
-	retunObject.pixelsPerUnit = (retunObject.scalePointGap * (retunObject.points.length - 1)) / retunObject.points[retunObject.points.length - 1];
+	returnObj.pixelsPerUnit = (returnObj.scalePointGap * (returnObj.points.length - 1)) / returnObj.points[returnObj.points.length - 1];
 
-	return retunObject;
+	return returnObj;
 }
 
 function getHighestReading(graphData) {
@@ -174,19 +172,105 @@ function log10(val) {
 }
 
 function drawGraph(canvas, graphData, scaleInfo) {
-	drawLabels(canvas, graphData);
 	drawAxis(canvas, graphData, scaleInfo);
 	drawData(canvas, graphData, scaleInfo);
 }
 
-function drawLabels(canvas, graphData) {
-
-}
-
 function drawAxis(canvas, graphData, scaleInfo) {
-	
+	// Get the context and the colours and fonts
+	var cxt = canvas.getContext('2d');
+	cxt.fillStyle = 'black';
+	cxt.font = '18pt verdana';
+
+	// y axis points
+	drawYLabels(canvas, cxt, scaleInfo, graphData)
+
+	// Sideways y axis label
+
+	// x axis values
+	drawXLabels(canvas, cxt, scaleInfo, graphData);
+
+	// x axis label 
+	cxt.textAlign = 'center';
+	cxt.fillText(graphData.xLabel, ((canvas.width - 100) / 2) + 100, canvas.height - 10, canvas.width - 100);
+
+	// axies
+	cxt.beginPath();
+	cxt.strokeStyle = 'black';
+	cxt.moveTo(100, 20);
+	cxt.lineTo(100, canvas.height - 80);
+	cxt.stroke();
+	cxt.moveTo(100, canvas.height - 80);
+	cxt.lineTo(canvas.width, canvas.height - 80);
+	cxt.stroke();
+	cxt.closePath();
+}	
+
+function drawYLabels(canvas, cxt, scaleInfo, graphData) {
+	var yValue = double = 20;
+
+	// Go through every point drawing them and a dash to show were it is on the scale
+	for (var i = scaleInfo.points.length - 1; i >= 0; i--, yValue += scaleInfo.pixelsBetweenScalePoints) {
+		// Draw a faint line along where the point it (making it easier to read)
+		if (i != 0) {
+			cxt.beginPath();
+			cxt.strokeStyle = 'gray';
+			cxt.moveTo(100, yValue);
+			cxt.lineTo(canvas.width, yValue);
+			cxt.stroke();
+			cxt.closePath();
+		}	
+
+		// Draw the dash
+		cxt.beginPath();
+		cxt.strokeStyle = 'black';
+		cxt.moveTo(100, yValue);
+		cxt.lineTo(90, yValue);
+		cxt.stroke();
+		cxt.closePath();
+
+		// Draw the label
+		cxt.textAlign = 'right';
+		cxt.textBaseLine = 'middle';
+		cxt.fillText(scaleInfo.points[i].toString(), 87, yValue + 8, 37);
+	}
 }
 
-function  drawData(canvas, graphData, scaleInfo) {
+function drawXLabels(canvas, cxt, scaleInfo, graphData) {
+	var xValue = double = 100 + scaleInfo.pixelsBetweenBars;
 
+	for (var i = 0; i < graphData.data.length; i++, xValue += scaleInfo.pixelsBetweenBars + scaleInfo.widthOfBars) {
+		// Draw the width of the bar
+		cxt.beginPath();
+		cxt.strokeStyle = 'black';
+		cxt.moveTo(xValue, canvas.height - 80);
+		cxt.lineTo(xValue, canvas.height - 70);
+		cxt.stroke();
+		cxt.moveTo(xValue + scaleInfo.widthOfBars, canvas.height - 80);
+		cxt.lineTo(xValue + scaleInfo.widthOfBars, canvas.height - 70);
+		cxt.stroke();
+		cxt.closePath();
+
+		// Draw the labels
+		cxt.textAlign = 'center';
+		cxt.textBaseLine = 'top';
+		cxt.fillText(graphData.data[i].field, xValue + (scaleInfo.widthOfBars / 2), canvas.height - 58, scaleInfo.widthOfBars);
+	}
+}
+
+function drawData(canvas, graphData, scaleInfo, gapToYAxis) {
+	// Get the context and the colours and fonts
+	var cxt = canvas.getContext('2d');
+	cxt.fillStyle = 'black';
+	cxt.font = '18pt verdana';
+
+	var xValue = double = 100 + scaleInfo.pixelsBetweenBars;
+
+	// Plot bar
+	for (var i = 0; i < graphData.data.length; i++, xValue += scaleInfo.pixelsBetweenBars + scaleInfo.widthOfBars) {
+		cxt.beginPath();
+		cxt.strokeStyle = 'black';
+		cxt.fillRect(xValue, canvas.height - 80, scaleInfo.widthOfBars, -(scaleInfo.pixelsPerUnit * graphData.data[i].count));
+		cxt.closePath();
+	}
 }
--- Javascript/DrawGraph.js
+++ Javascript/DrawGraph.js
@@ -2,8 +2,9 @@ var canvas = document.getElementById("canvas");
 
 // Load the JSON file
 $.getJSON('.\\JSON-Graph-Data\\Eye Colour Bar Graph.json', function (data) {
-	// Log all the infomation into the console
-	console.log(data);
+	// Display the title of the graph
+	var title = document.getElementById("title");
+	title.innerHTML = data.title;
 
 	// Runs the data as a bar graph if told to
 	if (data.graphType.toLowerCase() == "bar") {
