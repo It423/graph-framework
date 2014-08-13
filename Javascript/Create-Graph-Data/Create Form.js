586614af5093a29510aee5fe42d79471bd54c94e
It423 <it42@hotmail.co.uk>
2014-08-13 20:30:13 +0100

    Add remove reading button

--- Javascript/Create-Graph-Data/Create Form.js
+++ Javascript/Create-Graph-Data/Create Form.js
@@ -13,13 +13,18 @@ function loadPieChartForm() {
 	setTextInElement("unit", "<label>Unit: </label><input type='text' name='unitInput' style='width: 50%'></input>");
 
 	// Add the data fields
-	setTextInElement("data", "<h3>Readings:</h3>", true);
+	setTextInElement("data", "<h3>Readings:</h3>\n", true);
 	addPieField();
 
 	// Set the button to add data
 	var button = document.getElementById("add-new-data-reading");
 	button.innerHTML = "Add new Pie slice";
 	button.onclick = function() { addPieField() };
+	button.style.visibility = "visible";
+
+	button = document.getElementById("remove-data-reading");
+	button.innerHTML = "Remove Pie slice";
+	button.onclick = function() { removePieField() };
 	button.style.visibility = "visible";
 
 	// Make the validate button visable
@@ -32,13 +37,30 @@ function addPieField() {
 
 	// The input data
 	var pieField = [
-			"<h5 class='reading-pie' id='reading-name-" +idNum.toString() + "'><label>Field name: </label><input type='text' name='reading-name'></input></h5>",
-			"<h5 class='reading-pie' id='reading-value-" +idNum.toString() + "'><label>Value: </label><input type='text' name='reading-value' onkeypress='return isNumber(event)'></input></h5>",
-			"<h5 class='reading-pie' id='reading-colour-" +idNum.toString() + "'><label>Colour: </label><input type='text' name='reading-colour' onkeypress='return isNumber(event)'></input></h5>",
-			"<br/>",
-			"<br/>"].join("\n");
+			"<h5 class='reading-pie' id='reading-name-" + idNum.toString() + "'><label>Segment name: </label><input type='text' name='reading-name'></input></h5>",
+			"<h5 class='reading-pie' id='reading-value-" + idNum.toString() + "'><label>Value: </label><input type='text' name='reading-value' onkeypress='return isNumber(event)'></input></h5>",
+			"<h5 class='reading-pie' id='reading-colour-" + idNum.toString() + "'><label>Colour: </label><input type='text' name='reading-colour'></input></h5>",
+			"<br id='br-1-" + idNum.toString() + "'/>",
+			"<br id='br-2-" + idNum.toString() + "'/>\n"].join("\n");
 
 	setTextInElement("data", pieField, true);
+}
+
+function removePieField() {
+	// If there is not enough elements to remove, don't do anything
+	if (document.getElementById("data").innerHTML.split("</h5>").length <= 4) {
+		return false;
+	} else {
+		// Get the id number to remove the elements
+		var idNum = (($("form").serializeArray().length - 3) / 3) - 1;
+
+		// Remove the elements and the break
+		document.getElementById("reading-name-" + idNum.toString()).remove();
+		document.getElementById("reading-value-" + idNum.toString()).remove();
+		document.getElementById("reading-colour-" + idNum.toString()).remove();
+		document.getElementById("br-1-" + idNum.toString()).remove();
+		document.getElementById("br-2-" + idNum.toString()).remove();
+	}
 }
 
 function clearPage() {
