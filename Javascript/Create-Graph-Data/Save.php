69f7d53fcd37b3f67a9be7aed120ae3d3702a89c
It423 <it42@hotmail.co.uk>
2014-08-13 20:29:53 +0100

    Stop over writing of existing files by adding number in brakets to the end of the file name

--- Javascript/Create-Graph-Data/Save.php
+++ Javascript/Create-Graph-Data/Save.php
@@ -1,10 +1,38 @@
 <?php
-$myFile = "..\\..\\JSON-Graph-Data\\" . getFileName() . ".json";
+// The file name
+$dir = "..\\..\\JSON-Graph-Data\\";
+$fileName = getFileName();
+$extension = ".json";
+$myFile = $dir . $fileName . $extension;
 
-$fh = fopen($myFile, 'w');
+// If it exists add a number to the end (stops removal of graphs)
+if (file_exists($myFile)) {
+	$endingToFile = "(0)";
+	$numberInEnding = 0;
+	$newFileName = $dir . $fileName . $endingToFile . $extension;
 
+	while (file_exists($newFileName)) {
+		// Add one to the number in the ending
+		$numberInEnding++;
+
+		// Set the ending to the number surrounded by brackets
+		$endingToFile = "(" . (string)$numberInEnding . ")";
+
+		// Set the new file name to incude the number
+		$newFileName = $dir . $fileName . $endingToFile . $extension;
+	}
+
+	// Set the file name to the new one
+	$myFile = $newFileName;
+}
+
+// Open the file
+$fh = fopen($myFile, "w");
+
+// Write the data
 fwrite($fh, $_POST["myData"]);
 
+// Close the file
 fclose($fh);
 
 function getFileName() {
