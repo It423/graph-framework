e9a025db17f83270064ab212eda537a92c67b0f5
It423 <it42@hotmail.co.uk>
2014-08-20 17:50:56 +0100

    Store the list of file names as global variables and sort them into lists of graph types

--- Javascript/Search-Graphs/Check Directory.php
+++ Javascript/Search-Graphs/Check Directory.php
@@ -5,9 +5,11 @@ $fileNames = [];
 // Get all the files ending in ".json" within the graph data storage folder
 foreach (glob("..\\..\\JSON-Graph-Data\\*.json") as $file) {
 	// Add the filename to the array without the "..\\..\\JSON-Graph-Data" or ".json" parts
-	array_push($fileNames, str_replace("..\\..\\JSON-Graph-Data\\", "", str_replace(".json", "", $file)));
+	array_push($fileNames, str_replace("..\\..\\JSON-Graph-Data\\", "", $file));
 }
 
 // Send the list of file names back to the javascript
-echo json_encode($fileNames);
+foreach ($fileNames as $fileName) {
+	echo $fileName . ", ";
+}
 ?>
--- Javascript/Search-Graphs/Find Graphs.js
+++ Javascript/Search-Graphs/Find Graphs.js
@@ -1,9 +1,49 @@
+// Global variable of the graph file names
+var fileNames = [];
+
+// Global variables if the graph types and their corrosponding files' names
+var barFileNames = [];
+var pieFileNames = [];
+var lineFileNames = [];
+var cumulativeFileNames = [];
+var scatterFileNames = [];
+
 function findAllJSONGraphs() {
 	$.ajax({
 		type: "POST",
 		url: "Javascript\\Search-Graphs\\Check Directory.php",
-		success: function(fileNames) {
-			console.log(fileNames);
+		success: function(foundFileNames) {
+			// Split the file names into an array of them
+			fileNames = foundFileNames.split(", ");
+
+			// Remove the blank element 
+			fileNames.splice(fileNames.length - 1, 1);
+
+			// Put each graph type into its type and put it in that array
+			for (var i = 0; i < fileNames.length; i++) {
+				// Remove the ".json" from each filename
+				fileNames[i] = fileNames[i].replace(".json", "");
+
+				// Remove the brackets from the string if they are present
+				if (fileNames[i].charAt(fileNames[i].length - 1) == ')') {
+					fileNames[i] = fileNames[i].slice(0, fileNames[i].lastIndexOf('('));
+				}
+
+				// Sort the graph into its corrosponding graph type
+				if (fileNames[i].toLowerCase().lastIndexOf("bar graph") == fileNames[i].length - 9) {
+					barFileNames.push(fileNames[i]);
+				} else if (fileNames[i].toLowerCase().lastIndexOf("pie chart") == fileNames[i].length - 9) {
+					pieFileNames.push(fileNames[i]);
+				} else if (fileNames[i].toLowerCase().lastIndexOf("line graph") == fileNames[i].length - 10) {
+					if (fileNames[i].toLowerCase().lastIndexOf("cumulative line graph") == fileNames[i].length - 21) {
+						cumulativeFileNames.push(fileNames[i]);
+					} else {
+						lineFileNames.push(fileNames[i]);
+					}
+				} else if (fileNames[i].toLowerCase().lastIndexOf("scatter graph") == fileNames[i].length - 13) {
+					scatterFileNames.push(fileNames[i]);
+				}
+			}
 		},
 		error: function(e) {
 			console.log(e.message);
